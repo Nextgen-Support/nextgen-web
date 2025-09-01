63c23ce353c1d316477229906b534509c0c02573
Barata Apsii <apsiitese@gmail.com>
2025-09-01 13:06:35 +1000

    build settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,17 +3,23 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),  // <-- add this
+      '@': path.resolve(__dirname, 'src'),
     },
   },
   build: {
     outDir: 'dist',
     sourcemap: false,
+    emptyOutDir: true,
   },
   server: {
     open: true,
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
   },
 });
